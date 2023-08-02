b9669beedcc2baadcac0f78cd848331fbc2dd939
Glitch (glitch-hello-website) <none>
2023-08-02 13:17:31 +0000

    💗🌷 Checkpoint ./index.html:47020533/29210 ./layout/php/sosyalmedya.php:47020533/1536

---
+++ layout/php/sosyalmedya.php
@@ -0,0 +1,19 @@
+
+		<div class="col-md-2 col-xs-12 marginBottom20"></div>
+        <div class="socialindex col-md-4 col-xs-12 marginBottom20">
+            <div class="socialindexTit">DISCORD</div>
+            <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FEskisehirsporSKOfficial&amp;tabs=timeline&amp;width=486&amp;height=535&amp;small_header=true&amp;adapt_container_width=true&amp;hide_cover=false&amp;show_facepile=true&amp;appId=1199582510167327" width="100%" height="500" style="border:none;overflow:hidden;width:100%;" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+        </div>
+        <div class="socialindex col-md-4 col-xs-12 marginBottom20">
+            <div class="socialindexTit">TWITTER</div>
+            <a class="twitter-timeline"  href="https://twitter.com/Eskisehirspor" data-widget-id="251333466669580288" data-chrome="noheader nofooter">@Eskisehirspor tarafından gönderilen tweetler</a>
+            <script>!function (d, s, id) {
+                    var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https';
+                    if (!d.getElementById(id)) {
+                        js = d.createElement(s);
+                        js.id = id;
+                        js.src = p + "://platform.twitter.com/widgets.js";
+                        fjs.parentNode.insertBefore(js, fjs);
+                    }
+                }(document, "script", "twitter-wjs");</script>
+        </div>
